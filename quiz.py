73cf5e8de86042e547a2b4b237dfadd6f459e820
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2021-08-14 01:54:24 +0100

    feat: Create data structure(s) to store the questions and answers

--- quiz.py
+++ quiz.py
@@ -1,74 +1,67 @@
 """ A quiz program. """
 
-total_score = 0
-
-topic = input('Would you like art, or space questions? ')
-
-if topic.lower() == 'art':
-
-    print('Who painted the Mona Lisa?')
-    answer = input('Enter your answer: ')
-    if answer.lower() == 'vincent van gogh':
-        print('Correct!')
-        total_score += 1
-    else:
-        print('Sorry, the answer is Leonardo Da Vinci.')
+art_quiz_bank = [
+    {
+        "question": "Who painted the Mona Lisa?",
+        "answer": "vincent van gogh",
+    },
+    {
+        "question": "What precious stone is used to make the artist\'s pigment ultramarine?",
+        "answer": "lapiz lazuli",
+    },
+    {
+        "question": "Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?",
+        "answer": "chicago",
+    },
     
-    print('What precious stone is used to make the artist\'s pigment ultramarine?')
-    answer = input('Enter your answer: ')
-    if answer.lower() == 'lapiz lazuli':
-        print('Correct!')
-        total_score += 1
-    else:
-        print('Sorry, the correct answer is Lapiz lazuli.')
+]
 
-    print('Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?')
-    answer = input('Enter your answer: ')
-    if answer.lower() == 'chicago':
-        print('Correct!')
-        total_score += 1
-    else:
-        print('Sorry, the correct answer is Chicago.')
+space_quiz_bank = [
+    {
+        "question": "Which planet is closest to the sun?",
+        "answer": "mercury",
+    },
+    {
+        "question": "Which planet spins in the opposite direction to all the others in the solar system?",
+        "answer": "venus",
+    },
+    {
+        "question": "How many moons does Mars have?",
+        "answer": "2",
+    },
+    
+]
 
+
+def start_quiz(topic, quiz_question_bank):
+    """Starts Quiz"""
+    total_score = 0
+    number_of_quiz_questions = len(quiz_question_bank)
+    for question in quiz_question_bank:
+        print(question['question'])
+        answer = input('Enter your answer: ')
+        if answer.lower() == question['answer']:
+            print('Correct!')
+            total_score += 1
+        else:
+            print(f'Sorry, the answer is {question["answer"]}.')
+            
     print('End of quiz!')
-    print(f'Your total score on {topic} questions is {total_score} out of 3.')
-
-    if total_score == 3:
+    print(f'Your total score on {topic} questions is {total_score} out of {number_of_quiz_questions}.')
+    if total_score == number_of_quiz_questions:
         print('You got all the answers correct!')
+    
 
+    
+
+topic = input('Would you like art, or space questions?')
+
+if topic.lower() == "art":
+    start_quiz(topic, art_quiz_bank)
 
 elif topic.lower() == 'space':
-    
-    print('Which planet is closest to the sun?')
-    answer = input('Enter your answer: ')
-    if answer.lower() == 'mercury':
-        print('Correct!')
-        total_score += 1
-    else:
-        print('Sorry, the correct answer is Mercury.')
-
-    print('Which planet spins in the opposite direction to all the others in the solar system?')
-    answer = input('Enter your answer: ')
-    if answer.lower() == 'venus':
-        print('Correct!')
-        total_score += 1
-    else:
-        print('Sorry, the correct answer is Venus.')
-
-    print('How many moons does Mars have?')
-    answer = input('Enter your answer: ')
-    if answer == '2':
-        print('Correct!')
-        total_score += 1
-    else:
-        print('Sorry, the correct answer is 2.')
-
-    print('End of quiz!')
-    print(f'Your total score on {topic} questions is {total_score} out of 3.')
-
-    if total_score == 3:
-        print('You got all the answers correct!')
-
+    start_quiz(topic, space_quiz_bank)
+        
 else:
     print('That is not a valid topic. Restart the program to try again.')
 
